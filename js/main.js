cdad7d018072b60ca409d50389693b3ad393db63
Apurvsikka <102051299+Apurvsikka@users.noreply.github.com>
2023-03-25 00:10:51 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,4 +3,4 @@ import App from "./App.js";
 const root = document.getElementById("app");
 const app = new App(root);
 
-prompt('doble tap on a note to remove');
+alert('doble tap on a note to remove');
